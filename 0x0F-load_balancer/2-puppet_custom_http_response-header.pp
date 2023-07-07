786270856eed075d40028407f91b7964bf72dbd8
MildredJessica <nwonyejessica@gmail.com>
2023-07-07 03:25:39 -0700

    Changed the puppet file

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -3,21 +3,24 @@
 exec { 'update':
   provider => shell,
   command => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
 
 exec { 'install nginx':
-  provider => 'shell',
+  provider => shell,
   command => 'sudo apt-get install -y nginx',
+  before   => Exec['add_header'],
 }
 
 exec { 'add-header':
-  provider => 'shell',
+  provider => shell,
   environment => ['HOST=${hostname}'],
   command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before   => Exec['restart Nginx'],
 }
 
 exec {'restart nginx':
-  provider => 'shell',
+  provider => shell,
   command => 'sudo service restart nginx',
 }
 
