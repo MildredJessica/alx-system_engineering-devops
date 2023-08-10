6eae930610da49c17d3d117614c9a8ba275ee559
MildredJessica <nwonyejessica@gmail.com>
2023-08-10 09:11:27 +0100

    API advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API and returns the number of subscribers"""
+
+import requests as req
+
+
+def number_of_subscribers(subreddit):
+    """Queries the Reddit API to get the number of subscribers"""
+    url = "https://www.reddit.com/r{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101"
+    }
+    response = req.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    
+    return (response.json().get('data').get('subscribers'))
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""""Queries the Reddit API for a list containing the titles of all hot
+    articles for a given subreddit.
+"""
+
+import requests as req
+
+REDDIT = "https://www.reddit.com/"
+HEADERS = {'user-agent': 'my-app/0.0.1'}
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    '''
+        Recursive function that queries the Reddit API
+        Returns a list containing the titles of all hot articles
+        for a given subreddit.
+    '''
+    if after is None:
+        return hot_list
+
+    url = REDDIT + "r/{}/hot/.json".format(subreddit)
+
+    params = {
+        'limit': 100,
+        'after': after
+    }
+
+    response = req.get(url, headers=HEADERS, params=params, allow_redirects=False)
+
+    if response.status_code != 200:
+        return None
+
+    try:
+        js = response.json()
+
+    except ValueError:
+        return None
+
+    try:
+
+        data = js.get("data")
+        after = data.get("after")
+        children = data.get("children")
+        for child in children:
+            post = child.get("data")
+            hot_list.append(post.get("title"))
+
+    except Exception:
+        return None
+
+    return recurse(subreddit, hot_list, after)
